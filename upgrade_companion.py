532bd2673bccaf96ece10dbcc5ca6ea7041f625d
svk <oneindelijk@gmail.com>
2020-10-24 23:47:47 +0200

    terminal push

--- upgrade_companion.py
+++ upgrade_companion.py
@@ -20,21 +20,61 @@ from my_system import run_cmd, run_bg_cmd, prit
 
 expect_script = '/home/sam/systemen/systemen/intern/expect/pikaur_wrapper.expect'
 expect_output = '/home/sam/log/pikaur_wrapper-expect.log'
+started = None
+timeouts = 0
+def check_run_time():
+    # set starting time when not set
+    if started == None:
+        started = time.time()
+    if time.time() - started > 30*60:
+        timeouts ++
+        started = time.time()
+        return True
 
 def check_run():
     command = "ps ax | grep -v grep | grep -e 'expect'"
     result, error, code = run_cmd(command)
     if code == 0:
         return True
+
 def launch_expect():
     command = '/usr/bin/expect {}'.format(expect_script) #, expect_output)
     run_bg_cmd(command, expect_output, expect_output)
     print("Launched: {}".format(command))
 
-launch_expect()
+def execute(fn):
+    print(f'Executing {fn}') 
+    if fn == 'kill':
+        is_running = check_run()
+        if is_running:
+            print('Killing expect script')
+        else:
+            print('No expect script running...')
+
+
+def get_log(logfile):
+    with open(logfile) as FD:
+        command_results = FD.read()
+    return command_results.readlines()[-10:]
+    
+def check_log(logfile):
+    print('Checking result from expect script....')
+    last_part = get_log(logfile) # if 'OK' in last_part[-1]:
+    actions=[{'filter':'invalid command name','function':'kill'}]
+    for line in last_part:
+        for action in actions:
+            if action['filter'] in line:
+                execute(action['function'])
+        pass
+
+#launch_expect()
 while check_run():
     # waiting for expect to finish
     time.sleep(5)
     prit('=')
+    if check_run_time():
+        # install has exceeded timeout, check for errors
+        break;
 print()
+check_log(expect_output)
 print('expect has exited !')
